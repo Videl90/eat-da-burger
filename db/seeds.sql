69f51075fd14b22c4f23342eed3cc2aa23be94e4
Videl90 <lorena.sa.0117@gmail.com>
2020-07-07 14:41:25 -0500

    Created the connection, and database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lentil', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Falafel', false);
+
+
+
